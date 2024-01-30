d184534a89e5f10b5cd9d121abfe932fcede9bba
Luai Waleed <mistaluai@gmail.com>
2024-01-30 16:54:43 +0200

    solved interest-is-interesting

---
+++ interest-is-interesting/interest_is_interesting.go
@@ -0,0 +1,33 @@
+package interest
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	switch {
+	default:
+		return 3.213
+	case balance >= 0 && balance < 1000:
+		return 0.5
+	case balance >= 1000 && balance < 5000:
+		return 1.621
+	case balance >= 5000:
+		return 2.475
+	}
+}
+
+// Interest calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	return float64(InterestRate(balance)/100) * balance
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	return Interest(balance) + balance
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+	if balance >= targetBalance {
+		return 0
+	}
+	return 1 + YearsBeforeDesiredBalance(AnnualBalanceUpdate(balance), targetBalance)
+}
